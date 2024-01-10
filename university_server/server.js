3d11b4eb150c2a9745877305efcb7b2a67429837
Pratyaksh <pratyaksh0802@gmail.com>
2024-01-10 16:31:27 +0530

    Changed BAKCEND

--- university_server/server.js
+++ university_server/server.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(
   cors({
 
-    origin: "https://bike-dekho.vercel.app/",
+    //origin: "https://bike-dekho.vercel.app/",
     // origin: "http://127.0.0.1:5500", // this is for local development
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
